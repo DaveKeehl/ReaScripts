f126801c105dd6c2a8a489f091a3dd1e60c1d9af
Davide Ciulla <davide.ciulla@ticino.com>
2019-05-26 00:39:56 +0200

    added some error checks

--- Utilities/davekeehl_Go To Previous Top Level Track.lua
+++ Utilities/davekeehl_Go To Previous Top Level Track.lua
@@ -5,17 +5,30 @@
 
 function main()
 
-    currentProject = 0
+    reaper.ClearConsole()
 
+    currentProject = 0
     -- Get last touched track
     track = reaper.GetLastTouchedTrack()
     -- Get parent track
     parent = reaper.GetParentTrack(track)
+    -- Get total number of tracks
+    tracks = reaper.CountTracks(currentProject)
+    -- Get number of selected tracks
+    selected_tracks = reaper.CountSelectedTracks(currentProject)
 
-    -- Check if track has parent
-    if parent == nil
-        -- No parent --> Top level track
-        then
+    if tracks == 0 then
+        reaper.ShowConsoleMsg("This script needs tracks to work with. Create a few first.")
+    end
+
+    if selected_tracks == 0 then
+        reaper.ShowConsoleMsg("You need to select one track.")
+    elseif selected_tracks > 1 then
+        reaper.ShowConsoleMsg("Please select only one track.")
+    else
+        -- Check if track has parent
+        if parent == nil then
+            -- No parent --> Top level track
             -- Go to next track
             reaper.Main_OnCommandEx(40286, 0, currentProject)
             track = reaper.GetLastTouchedTrack()
@@ -23,22 +36,21 @@ function main()
             parent = reaper.GetParentTrack(track)
             -- Check if this track is a child
             -- If it is, iterate through all the children
-            while (parent ~= nil)
-                do
-                    -- Go to next track
-                    reaper.Main_OnCommandEx(40286, 0, currentProject)
-                    track = reaper.GetLastTouchedTrack()
-                    parent = reaper.GetParentTrack(track)
-                end
+            while (parent ~= nil) do
+                -- Go to next track
+                reaper.Main_OnCommandEx(40286, 0, currentProject)
+                track = reaper.GetLastTouchedTrack()
+                parent = reaper.GetParentTrack(track)
+            end
         -- Has parent --> Not top level track
         else
-            while (parent ~= nil)
-                do
-                    -- Go to next track
-                    reaper.Main_OnCommandEx(40286, 0, currentProject)
-                    track = reaper.GetLastTouchedTrack()
-                    parent = reaper.GetParentTrack(track)
-                end
+            while (parent ~= nil) do
+                -- Go to next track
+                reaper.Main_OnCommandEx(40286, 0, currentProject)
+                track = reaper.GetLastTouchedTrack()
+                parent = reaper.GetParentTrack(track)
+            end
+        end
     end
 
 end
