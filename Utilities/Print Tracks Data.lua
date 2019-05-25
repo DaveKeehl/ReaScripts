bae11dded4ec94297853d2e260b77d55ee4543fd
Davide Ciulla <davide.ciulla@ticino.com>
2019-05-25 16:59:28 +0200

    new set of changes

--- Utilities/Print Tracks Data.lua
+++ Utilities/Print Tracks Data.lua
@@ -28,7 +28,7 @@ function printTrackInfo()
   currentProject = 0
   tracks = reaper.CountTracks(currentProject)
   selected_tracks = reaper.CountSelectedTracks(currentProject)
-  description = "MODE 1: ALL TRACKS" .. "\n"
+  description = "SCRIPT MODE: ALL TRACKS" .. "\n"
   mode = "all"
 
   if tracks == 0
@@ -52,11 +52,10 @@ function printTrackInfo()
       reaper.ShowConsoleMsg("---------------------------------------------------" .. "\n")
 
       -- CHOOSE SCRIPT MODE
-      if selected_tracks > 0
-        then
-          description = "MODE 2: ONLY SELECTED TRACKS" .. "\n"
-          tracks = selected_tracks
-          mode = "selected"
+      if selected_tracks > 0 then
+        description = "SCRIPT MODE: ONLY SELECTED TRACKS" .. "\n"
+        tracks = selected_tracks
+        mode = "selected"
       end
       setScriptMode(description, tracks, mode)
 
@@ -94,60 +93,60 @@ function getTracksInfo(track)
     
     -- TRACK NUMBER
     track_number = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
-    reaper.ShowConsoleMsg("TRACK " .. math.floor(track_number) .. "\n")
+    reaper.ShowConsoleMsg("*** TRACK " .. math.floor(track_number) .. " ***\n")
 
     -- TRACK NAME
     _, name = reaper.GetTrackName(track)
-    reaper.ShowConsoleMsg("Name: " .. name .. "\n")
+    reaper.ShowConsoleMsg("- Name: " .. name .. "\n")
 
     -- PARENT TRACK
     parent = reaper.GetParentTrack(track)
     if parent == nil
       then
-        reaper.ShowConsoleMsg("Parent: NONE\n")
+        reaper.ShowConsoleMsg("- Parent: None\n")
       else
         _, parent_name = reaper.GetTrackName(parent)
-        reaper.ShowConsoleMsg("Parent: " .. parent_name .. "\n")
+        reaper.ShowConsoleMsg("- Parent: " .. parent_name .. "\n")
     end
 
     -- TRACK DEPTH
     depth = reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
-    reaper.ShowConsoleMsg("Track depth: " .. tostring(depth) .. "\n")
+    reaper.ShowConsoleMsg("- Depth: " .. tostring(math.floor(depth)) .. "\n")
     
     -- IS SELECTED
     isSelected = reaper.GetMediaTrackInfo_Value(track, "I_SELECTED")
     if isSelected == 0
       then
-      reaper.ShowConsoleMsg("Selected: No\n")
+      reaper.ShowConsoleMsg("- Selected: No\n")
       else
-      reaper.ShowConsoleMsg("Selected: Yes\n")
+      reaper.ShowConsoleMsg("- Selected: Yes\n")
     end
 
     -- SOLO
     solo_mode = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
     if solo_mode == 0
       then
-        reaper.ShowConsoleMsg("Solo: OFF\n")
+        reaper.ShowConsoleMsg("- Solo: OFF\n")
       else
-        reaper.ShowConsoleMsg("Solo: ON\n")
+        reaper.ShowConsoleMsg("- Solo: ON\n")
     end
     
     -- MUTE
     isMute = reaper.GetMediaTrackInfo_Value(track, "B_MUTE")
     if isMute == 0
       then
-        reaper.ShowConsoleMsg("Mute: OFF\n")
+        reaper.ShowConsoleMsg("- Mute: OFF\n")
       else
-        reaper.ShowConsoleMsg("Mute: ON\n")
+        reaper.ShowConsoleMsg("- Mute: ON\n")
     end
 
     -- PHASE
     hasPhaseInverted = reaper.GetMediaTrackInfo_Value(track, "B_PHASE")
     if hasPhaseInverted == 0
       then
-      reaper.ShowConsoleMsg("Phase: Normal\n")
+      reaper.ShowConsoleMsg("- Phase: Normal\n")
       else
-      reaper.ShowConsoleMsg("Phase: Inverted\n")
+      reaper.ShowConsoleMsg("- Phase: Inverted\n")
     end
 
 end
